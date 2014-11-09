46842ec29a50b8552a40e02e99062178bdbc0454
dhleong <falcone88@gmail.com>
2014-11-09 15:12:57 -0500

    Ensure commands are executed on dispatch thread
    It works! Testing with http client does the right thing

--- core/src/org/intellivim/CommandExecutor.java
+++ core/src/org/intellivim/CommandExecutor.java
@@ -2,6 +2,8 @@ package org.intellivim;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.application.ModalityState;
 
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -23,13 +25,27 @@ public class CommandExecutor {
         return execute(new InputStreamReader(json));
     }
 
-    public Result execute(Reader json) {
+    public Result execute(final Reader json) {
         try {
-            ICommand command = gson.fromJson(json, ICommand.class);
-            if (command == null)
-                return SimpleResult.error("Invalid command");
+            final Result[] result = new Result[1];
 
-            return command.execute();
+            // we must execute on the event dispatcher thread
+            ApplicationManager.getApplication().invokeAndWait(new Runnable() {
+
+                @Override
+                public void run() {
+
+                    final ICommand command = gson.fromJson(json, ICommand.class);
+                    if (command == null) {
+                        result[0] = SimpleResult.error("Invalid command");
+                        return;
+                    }
+
+                    result[0] = command.execute();
+                }
+            }, ModalityState.any());
+
+            return result[0];
         } catch (JsonSyntaxException e) {
             Throwable cause = e.getCause();
             final Throwable actual = cause == null ? cause : e;
