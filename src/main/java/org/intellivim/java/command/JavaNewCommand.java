86a67eeef9ba309dfcc9ce8155789c7cf4d5a78c
dhleong <falcone88@gmail.com>
2015-04-01 09:59:38 -0400

    Finish initial JavaNewCommand implementation
    Refs #22

--- src/main/java/org/intellivim/java/command/JavaNewCommand.java
+++ src/main/java/org/intellivim/java/command/JavaNewCommand.java
@@ -1,6 +1,10 @@
 package org.intellivim.java.command;
 
+import com.intellij.ide.util.DirectoryUtil;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.Computable;
+import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.JavaDirectoryService;
 import com.intellij.psi.JavaPsiFacade;
 import com.intellij.psi.PsiClass;
@@ -8,6 +12,9 @@ import com.intellij.psi.PsiDirectory;
 import com.intellij.psi.PsiFile;
 import com.intellij.psi.PsiJavaFile;
 import com.intellij.psi.PsiPackage;
+import com.intellij.psi.impl.file.PsiDirectoryFactory;
+import com.intellij.util.Function;
+import com.intellij.util.containers.ContainerUtil;
 import org.intellivim.Command;
 import org.intellivim.ProjectCommand;
 import org.intellivim.Required;
@@ -16,9 +23,26 @@ import org.intellivim.SimpleResult;
 import org.intellivim.core.command.find.LocationResult;
 import org.intellivim.inject.Inject;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
 /**
+ * If there are multiple possible directories to place the
+ *  file in, we will return <code>{dirs: [...]}</code>.
+ *  The chosen directory should be provided in a new
+ *  command in the "dir" field, including all the same
+ *  previous fields as well.
+ *
+ * If `file` is provided, and no packages are used in
+ *  `name`, we will put the new class in the same package
+ *  with the current file. This is slightly different
+ *  from IntelliJ (which just appends packages to the
+ *  current package), but without making an extra command,
+ *  or making the command needlessly complicated, there's
+ *  no good way to specify an absolute package.
+ *
  * @author dhleong
  */
 @Command("java_new")
@@ -36,6 +60,7 @@ public class JavaNewCommand extends ProjectCommand {
     @Required String name;
 
     /* Optional */@Inject PsiFile file;
+    /* Optional */String dir;
 
     public JavaNewCommand(final Project project, final String type, final String name) {
         super(project);
@@ -46,7 +71,15 @@ public class JavaNewCommand extends ProjectCommand {
 
     @Override
     public Result execute() {
-        // FIXME there may be multiple source directories...
+        return ApplicationManager.getApplication().runWriteAction(new Computable<Result>() {
+            @Override
+            public Result compute() {
+                return doExecute();
+            }
+        });
+    }
+
+    Result doExecute() {
         final PsiDirectory[] dirs = resolveNewDirectory();
         final PsiDirectory dir;
         if (dirs == null) {
@@ -54,11 +87,24 @@ public class JavaNewCommand extends ProjectCommand {
         } else if (dirs.length == 0) {
             return SimpleResult.error("Unable to resolve package");
         } else if (dirs.length > 1) {
-             // FIXME disambiguate directories
-            dir = dirs[0];
+            dir = resolveChosenDirectory(dirs);
+            if (dir == null) {
+                // ambiguous; let the user resolve it
+                return SimpleResult.success(
+                        ContainerUtil.newHashMap(
+                                Collections.singletonList("dirs"),
+                                Collections.singletonList(mapDirs(dirs))
+                        )
+                );
+            }
         } else {
+            // unambiguous
             dir = dirs[0];
         }
+
+        // be up to date, please
+        // (if we delete outside of IntelliJ, it gets confused)
+        dir.getVirtualFile().refresh(false, true);
 
         final String templateName = resolveTemplateName();
         final String className = resolveClassName();
@@ -67,30 +113,88 @@ public class JavaNewCommand extends ProjectCommand {
         return SimpleResult.success(new LocationResult(created));
     }
 
-    protected PsiDirectory[] resolveNewDirectory() {
-        final JavaPsiFacade psiFacade = JavaPsiFacade.getInstance(project);
-        final int lastPackageEnd = name.lastIndexOf('.');
-        if (-1 != lastPackageEnd) {
-            // separators included; assume that's the package
-            final String packageName = name.substring(0, lastPackageEnd);
-            PsiPackage psiPackage = psiFacade.findPackage(packageName);
-            if (psiPackage != null) {
-                return psiPackage.getDirectories();
-            } else {
-                // create it?
+    protected PsiDirectory resolveChosenDirectory(PsiDirectory[] choices) {
+        if (StringUtil.isEmpty(dir)) {
+            // user didn't provide a choice
+            return null;
+        }
+
+        for (final PsiDirectory psiDirectory : choices) {
+            if (dir.equals(psiDirectory.getVirtualFile().getPath())) {
+                return psiDirectory;
             }
         }
 
-        if (file instanceof PsiJavaFile) {
-            // same as the current file
-//            return file.getContainingDirectory();
-            return psiFacade.findPackage(((PsiJavaFile) file).getPackageName()).getDirectories();
+        // not a valid choice
+        return null;
+    }
+
+    protected PsiDirectory[] resolveNewDirectory() {
+        final JavaPsiFacade psiFacade = JavaPsiFacade.getInstance(project);
+        final int lastPackageEnd = name.lastIndexOf('.');
+        final String packageName = name.substring(0, lastPackageEnd);
+        if (-1 != lastPackageEnd) {
+            // separators included; assume that's the package
+            final PsiPackage psiPackage = psiFacade.findPackage(packageName);
+            if (psiPackage != null) {
+                return psiPackage.getDirectories();
+            }
         }
 
-//        return project.getp
-//        PsiManager.getInstance(project).dir
-        // FIXME all of this
-        return psiFacade.findPackage("").getDirectories();
+
+        if (file instanceof PsiJavaFile) {
+            // same as the current file
+            final PsiPackage psiPackage = psiFacade.findPackage(
+                    ((PsiJavaFile) file).getPackageName());
+            if (psiPackage != null) {
+                return psiPackage.getDirectories();
+            }
+        }
+
+        final PsiPackage defaultPackage = psiFacade.findPackage("");
+        if (defaultPackage == null) {
+            throw new IllegalArgumentException("Unable to determine default package");
+        }
+
+        final PsiDirectory[] sourceDirectories = defaultPackage.getDirectories();
+        if (-1 == lastPackageEnd) {
+            // default package
+            return sourceDirectories;
+        }
+
+        if (!PsiDirectoryFactory.getInstance(project).isValidPackageName(packageName)) {
+            throw new IllegalArgumentException(packageName + " is not a valid package name");
+        }
+
+        final List<PsiDirectory> packageDirs =
+                new ArrayList<PsiDirectory>(sourceDirectories.length);
+
+        for (final PsiDirectory srcRoot : sourceDirectories) {
+            // TODO If we created a directory here but don't use it, we should clean up after
+            final PsiDirectory dir = DirectoryUtil
+                    .createSubdirectories(packageName, srcRoot, ".");
+            packageDirs.add(dir);
+        }
+
+        return packageDirs.toArray(sourceDirectories);
+    }
+
+    protected String resolveTemplateName() {
+        final String resolved = TYPES_MAP.get(type);
+        if (resolved == null) {
+            throw new IllegalArgumentException("No such type: " + type);
+        }
+
+        return resolved;
+    }
+
+    private List<String> mapDirs(final PsiDirectory[] dirs) {
+        return ContainerUtil.map(dirs, new Function<PsiDirectory, String>() {
+            @Override
+            public String fun(final PsiDirectory psiDirectory) {
+                return psiDirectory.getVirtualFile().getPath();
+            }
+        });
     }
 
     private String resolveClassName() {
@@ -103,12 +207,4 @@ public class JavaNewCommand extends ProjectCommand {
         return name.substring(lastPackageEnd + 1);
     }
 
-    protected String resolveTemplateName() {
-        final String resolved = TYPES_MAP.get(type);
-        if (resolved == null) {
-            throw new IllegalArgumentException("No such type: " + type);
-        }
-
-        return resolved;
-    }
 }
--- src/test/java/org/intellivim/java/command/JavaNewTest.java
+++ src/test/java/org/intellivim/java/command/JavaNewTest.java
@@ -1,5 +1,8 @@
 package org.intellivim.java.command;
 
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.psi.PsiDirectory;
 import org.intellivim.BaseTestCase;
 import org.intellivim.SimpleResult;
 import org.intellivim.core.command.find.LocationResult;
@@ -7,6 +10,8 @@ import org.intellivim.core.command.find.LocationResult;
 import java.io.File;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * @author dhleong
@@ -18,8 +23,31 @@ public class JavaNewTest extends BaseTestCase {
         return getProjectPath(JAVA_PROJECT);
     }
 
+    public void testResolveDir() {
+        final String mainDir = "src/java/main";
+        final String testDir = "src/java/test";
+        final PsiDirectory srcMain = mockDir(mainDir);
+        final PsiDirectory srcTest = mockDir(testDir);
+        final PsiDirectory[] choices = {srcMain, srcTest};
+
+        assertThat(commandWithDir(null).resolveChosenDirectory(choices))
+                .isNull();
+
+        assertThat(commandWithDir(mainDir).resolveChosenDirectory(choices))
+                .isSameAs(srcMain);
+
+        assertThat(commandWithDir(testDir).resolveChosenDirectory(choices))
+                .isSameAs(srcTest);
+    }
+
     public void testNewFQN() {
-        SimpleResult result = (SimpleResult) new JavaNewCommand(getProject(),
+
+        final Project project = getProject();
+        final File expectedDir = new File(project.getBaseDir().getPath(),
+                "src/org/intellivim/test");
+        assertThat(expectedDir).doesNotExist();
+
+        final SimpleResult result = (SimpleResult) new JavaNewCommand(project,
                 "class", "org.intellivim.test.NewlyCreated").execute();
         assertSuccess(result);
 
@@ -27,10 +55,32 @@ public class JavaNewTest extends BaseTestCase {
         System.out.println("Created: " + location);
         assertThat(location).isNotNull();
 
-        File createdFile = new File(location.file);
-        assertThat(createdFile).exists();
+        assertThat(expectedDir).exists();
+
+        final File createdFile = new File(location.file);
+        assertThat(createdFile)
+                .exists()
+                .hasParent(expectedDir)
+                .hasName("NewlyCreated.java");
 
         // cleanup
         createdFile.delete();
+        expectedDir.delete();
+    }
+
+    private static PsiDirectory mockDir(final String path) {
+        final VirtualFile virtual = mock(VirtualFile.class);
+        when(virtual.getPath()).thenReturn(path);
+
+        final PsiDirectory dir = mock(PsiDirectory.class);
+        when(dir.getVirtualFile()).thenReturn(virtual);
+        return dir;
+    }
+
+    JavaNewCommand commandWithDir(final String dir) {
+        final JavaNewCommand command = new JavaNewCommand(getProject(),
+                "class", "org.intellivim.test.NewlyCreated");
+        command.dir = dir;
+        return command;
     }
 }
